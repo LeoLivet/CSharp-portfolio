a9fdd959c6dcbc5750d21cad92615ce97e919fd1
LeoLivet <161572573+LeoLivet@users.noreply.github.com>
2024-08-27 15:59:01 +0200

    Added code project 2 and some some thoughts

--- Snippets.cs
+++ Snippets.cs
@@ -343,7 +343,11 @@ Console.WriteLine($"Product: {size} {color} {type}");
 #endregion
 
 
-#region Console readline project. Pick an int between 5-10
+
+
+
+
+#region Code project 1 Console readline project. Pick an int between 5-10
 
 string? thenumberAsAString;
 int theNumber = 0;
@@ -376,3 +380,56 @@ do
 }while(validNumber == false);
 
 #endregion
+
+
+
+
+
+
+#region Code project 2 check user credentials
+/* Need to redo this i think theres a way to simply check if  
+the userInput matches something in the array and then do
+ a switch case instead */
+
+string? userInput;
+string? trimmedString;
+bool userHasValidCredentials = false;
+string[] levelCredentials = {"administrator", "manager", "user"};
+
+
+
+Console.WriteLine("Enter your role name (Administrator, Manager or User)");
+
+
+do {
+userInput = Console.ReadLine();
+trimmedString = userInput.Trim().ToLower();
+
+if(trimmedString == levelCredentials[0])
+ {
+    Console.WriteLine("You are an Administrator");
+    userHasValidCredentials = true;
+ }
+
+else if(trimmedString == levelCredentials[1])
+{
+    Console.WriteLine("You are a Manager");
+    userHasValidCredentials = true;
+}
+
+else if(trimmedString == levelCredentials[2])
+{
+    Console.WriteLine("You are a user");
+    userHasValidCredentials = true;
+}
+
+else
+{
+    Console.WriteLine($"The credential you have entered:{userInput} is not valid");
+}
+
+}while(userHasValidCredentials != true);
+#endregion
+
+
+
