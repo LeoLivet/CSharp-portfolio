60563ca54c1016f051589cf792a88406094e2ef9
LeoLivet <161572573+LeoLivet@users.noreply.github.com>
2024-09-12 13:45:44 +0200

    Added parse method challenge

--- Snippets.cs
+++ Snippets.cs
@@ -1133,3 +1133,71 @@ Console.WriteLine($"double  : {double.MinValue} to {double.MaxValue} (with ~15-1
 Console.WriteLine($"decimal : {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
 
 #endregion
+
+
+
+
+
+
+
+#region combining string array values as strings and as integers challenge
+
+// My Solution
+
+using System.Globalization;
+CultureInfo.CurrentCulture = new CultureInfo("en-US");
+
+
+
+string[] values = {"12.3", "45", "ABC", "11", "DEF"};
+decimal numbers = 0;
+string itsText = "";
+decimal itsNumeric = 0;
+for (int i = 0; i < values.Length; i++)
+{
+	if (decimal.TryParse(values[i], out numbers))
+	{
+		itsNumeric += numbers;
+	}
+	
+	else
+	{
+		itsText += values[i];
+	}
+}
+
+
+
+Console.WriteLine(itsNumeric);
+Console.WriteLine(itsText);
+
+
+// Microsoft solution
+
+
+string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+
+decimal total = 0m;
+string message = "";
+
+foreach (var value in values)
+{
+    decimal number; // stores the TryParse "out" value
+    if (decimal.TryParse(value, out number))
+    {
+        total += number;
+    } else
+    {
+        message += value;
+    }
+}
+
+Console.WriteLine($"Message: {message}");
+Console.WriteLine($"Total: {total}");
+
+
+
+
+
+
+#endregion
