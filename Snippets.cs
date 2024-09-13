7822d49fe53632b1613b85e4957798b9b4f48f88
LeoLivet <161572573+LeoLivet@users.noreply.github.com>
2024-09-13 13:30:07 +0200

    Added array sort excersize

--- Snippets.cs
+++ Snippets.cs
@@ -1352,3 +1352,43 @@ Console.WriteLine(result);
 
 #endregion
 
+#region Exercise - Complete a challenge to parse a string of orders, sort the orders and tag possible errors
+// my solution
+
+//promise i didnt peak at the solution
+string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+
+string[] items = orderStream.Split(",");
+Array.Sort(items);
+
+foreach (var item in items)
+{
+	if(item.Length != 4)
+	{
+		Console.WriteLine($"{item}\t\t- Error");
+	}
+	
+	else
+	Console.WriteLine(item);
+}
+
+//microsoft solution
+
+string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+string[] items = orderStream.Split(',');
+Array.Sort(items);
+
+foreach (var item in items)
+{
+	if (item.Length == 4)
+	{
+		Console.WriteLine(item);
+	}
+	else
+	{
+		Console.WriteLine(item + "\t- Error");
+	}
+}
+
+
+#endregion
