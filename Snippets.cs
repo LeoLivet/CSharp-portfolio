73048b644efbbdce9e955b3dbd6b42696f4544b3
LeoLivet <161572573+LeoLivet@users.noreply.github.com>
2024-09-13 12:51:23 +0200

    Added reverse words challenge

--- Snippets.cs
+++ Snippets.cs
@@ -1207,8 +1207,7 @@ Console.WriteLine($"Total: {total}");
 
 
 
-#region output math operations as specific number types challenge
-
+#region Exercise - Complete a challenge to reverse words in a sentence
 
 // My solution
 int value1 = 11;
@@ -1286,3 +1285,70 @@ Console.WriteLine($"Divide value3 by value1, display the result as a float: {res
 
 
 #endregion
+
+
+
+
+
+#region Perform operations on arrays using helper methods in C#
+
+
+
+// my solution:
+//thoughts: i ended up peeking at the solution after i had managed to reverse
+//each word but couldn't get further when trying to put them back in an array, i was trying/practicing 
+//foreach loops but couldnt get my reversedword put back into a new string array because i had a better understanding of the loop while using for instead.
+
+using System.Runtime.InteropServices;
+
+string pangram = "The quick brown fox jumps over the lazy dog";
+
+
+string[] pangramWordArray = pangram.Split(" ");
+
+string[] resultMessage = new string[pangramWordArray.Length];
+
+Console.WriteLine(pangramWordArray[0]);
+// string[] solutionArray = new string(resultWordString)
+
+for(int i = 0; i < pangramWordArray.Length; i++)
+{
+	char[] reverseword = pangramWordArray[i].ToCharArray();
+	Array.Reverse(reverseword);
+	resultMessage[i] = new string(reverseword);
+	// Console.WriteLine(resultMessage[i]);
+	// Console.WriteLine(reverseword);
+}
+string reversedWordsNotAll = String.Join(" ", resultMessage);
+Console.WriteLine(reversedWordsNotAll);
+
+
+
+
+
+
+//microsofts solution:
+
+
+string pangram = "The quick brown fox jumps over the lazy dog";
+
+// Step 1
+string[] message = pangram.Split(' ');
+
+//Step 2
+string[] newMessage = new string[message.Length];
+
+// Step 3
+for (int i = 0; i < message.Length; i++)
+{
+	char[] letters = message[i].ToCharArray();
+	Array.Reverse(letters);
+	newMessage[i] = new string(letters);
+}
+
+//Step 4
+string result = String.Join(" ", newMessage);
+Console.WriteLine(result);
+
+#endregion
+
