ed103f1382be0dd3bfe5074c09132e6782bf645d
LeoLivet <161572573+LeoLivet@users.noreply.github.com>
2024-09-17 11:54:24 +0200

    Added string interpolation exercise

--- Snippets.cs
+++ Snippets.cs
@@ -1352,6 +1352,10 @@ Console.WriteLine(result);
 
 #endregion
 
+
+
+
+
 #region Exercise - Complete a challenge to parse a string of orders, sort the orders and tag possible errors
 // my solution
 
@@ -1389,6 +1393,76 @@ foreach (var item in items)
 		Console.WriteLine(item + "\t- Error");
 	}
 }
+
+
+#endregion
+
+
+
+
+
+#region Exercise - Complete a challenge to apply string interpolation to a form letter
+
+string customerName = "Ms. Barros";
+
+string currentProduct = "Magic Yield";
+
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
+
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
+
+Console.WriteLine($"Dear {customerName},");
+Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.\n");
+Console.WriteLine($"Currently, you own {currentShares:C} shares at a return of {currentReturn:P}\n");
+Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P2}. Given your current volume, your potential profit would be {newProfit:C}");
+
+Console.WriteLine("Here's a quick comparison: \n");
+Console.WriteLine($"{currentProduct}\t\t {currentReturn:P2}\t{currentProfit:C}");
+Console.WriteLine($"{newProduct}\t\t {newReturn:P2}\t{newProfit:C}");
+string comparisonMessage = "";
+
+// my logic here
+
+Console.WriteLine(comparisonMessage);
+
+
+//microsoft solution
+
+string customerName = "Ms. Barros";
+
+string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
+
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
+
+Console.WriteLine($"Dear {customerName},");
+Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.\n");
+Console.WriteLine($"Currently, you own {currentShares:N} shares at a return of {currentReturn:P}.\n");
+Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P}.  Given your current volume, your potential profit would be {newProfit:C}.\n");
+
+Console.WriteLine("Here's a quick comparison:\n");
+
+string comparisonMessage = "";
+
+comparisonMessage = currentProduct.PadRight(20);
+comparisonMessage += String.Format("{0:P}", currentReturn).PadRight(10);
+comparisonMessage += String.Format("{0:C}", currentProfit).PadRight(20);
+
+comparisonMessage += "\n";
+comparisonMessage += newProduct.PadRight(20);
+comparisonMessage += String.Format("{0:P}", newReturn).PadRight(10);
+comparisonMessage += String.Format("{0:C}", newProfit).PadRight(20);
+
+Console.WriteLine(comparisonMessage);
+
 
 
 #endregion
