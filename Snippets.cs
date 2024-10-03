82d5110556d9cf7ffff600e725dc78104c28c96d
LeoLivet <161572573+LeoLivet@users.noreply.github.com>
2024-10-03 12:30:42 +0200

    Added Exercise - Complete the challenge to display email addresses. :celebrateemoji: :)

--- Snippets.cs
+++ Snippets.cs
@@ -1468,9 +1468,10 @@ Console.WriteLine(comparisonMessage);
 #endregion
 
 
-//Mine:
 
 #region Exercise - Complete a challenge to extract, replace, and remove data from an input string
+
+//Mine:
 
 const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
 
@@ -1596,13 +1597,116 @@ TellFortune();
 
 void TellFortune() 
 {
-    Console.WriteLine("A fortune teller whispers the following words:");
-    string[] fortune = (luck > 75 ? good : (luck < 25 ? bad : neutral));
-    for (int i = 0; i < 4; i++) 
-    {
-        Console.Write($"{text[i]} {fortune[i]} ");
-    }
+	Console.WriteLine("A fortune teller whispers the following words:");
+	string[] fortune = (luck > 75 ? good : (luck < 25 ? bad : neutral));
+	for (int i = 0; i < 4; i++) 
+	{
+		Console.Write($"{text[i]} {fortune[i]} ");
+	}
 }
 
 #endregion
 
+
+
+
+
+#region Exercise - Complete the challenge to display email addresses
+// this is my solution. For once i felt that i have managed to make something that seems to make somewhat sense without
+// being a total hackjob. I think this might be my first feeling of solving a problem and damn it feels good!
+
+// here is my solution:
+
+string[,] corporate = 
+{
+	{"Robert", "Bavin"},
+	{"Simon", "Bright"},
+	{"Kim", "Sinclair"}, 
+	{"Aashrita", "Kamath"},
+	{"Sarah", "Delucchi"}, 
+	{"Sinan", "Ali"}
+};
+
+string[,] external = 
+{
+	{"Vinnie", "Ashton"}, 
+	{"Cody", "Dysart"},
+	{"Shay", "Lawrence"}, 
+	{"Daren", "Valdes"}
+};
+
+/*
+TODO Your challenge is to create a method that displays the correct email address for both internal and external employees
+*/
+
+string corporateDomain = "contoso.com";
+string externalDomain = "hayworth.com";
+
+
+for (int i = 0; i < corporate.GetLength(0); i++) 
+{
+	DisplayMail(firstName: corporate[i,0], lastName:corporate[i,1], location: "corporate" );
+}
+
+Console.WriteLine();
+
+for (int i = 0; i < external.GetLength(0); i++) 
+{
+	DisplayMail(external[i,0], external[i,1], "external");
+}
+
+
+void DisplayMail(string firstName, string lastName, string location)
+{
+	firstName = firstName.Substring(0,2).ToLower();
+	lastName = lastName.ToLower();
+	string userName = firstName + lastName;
+	string domain = (location == "corporate" ) ? corporateDomain : externalDomain;
+	string newEmail = userName + "@" + domain;
+
+
+	
+	
+	
+	Console.WriteLine(newEmail);
+	
+}
+
+
+//And heres the Proposed microsoft solution:
+
+string[,] corporate = 
+	{
+		{"Robert", "Bavin"}, {"Simon", "Bright"},
+		{"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+		{"Sarah", "Delucchi"}, {"Sinan", "Ali"}};
+
+	string[,] external = 
+	{
+		{"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+		{"Shay", "Lawrence"}, {"Daren", "Valdes"}
+	};
+
+	string externalDomain = "hayworth.com";
+
+	for (int i = 0; i < corporate.GetLength(0); i++) 
+	{
+		DisplayEmail(first: corporate[i,0], last: corporate[i,1]);
+	}
+
+	for (int i = 0; i < external.GetLength(0); i++) 
+	{
+		DisplayEmail(first: external[i,0], last: external[i,1], domain: externalDomain);
+	}
+
+	void DisplayEmail(string first, string last, string domain = "contoso.com") 
+	{
+		string email = first.Substring(0, 2) + last;
+		email = email.ToLower();
+		Console.WriteLine($"{email}@{domain}");
+	}
+	
+	//I had trouble somewhere midway because of not fully understanding the text, think i also had to add the emails as an array. But that comes later! :)) :celebrateemoji:
+
+
+#endregion
