a8a29c92f7684f8e7af5eb8560c0dff3fc665baf
LeoLivet <161572573+LeoLivet@users.noreply.github.com>
2024-10-09 15:26:09 +0200

    Added dicegame excersize

--- Snippets.cs
+++ Snippets.cs
@@ -1469,6 +1469,8 @@ Console.WriteLine(comparisonMessage);
 
 
 
+
+
 #region Exercise - Complete a challenge to extract, replace, and remove data from an input string
 
 //Mine:
@@ -1612,6 +1614,7 @@ void TellFortune()
 
 
 #region Exercise - Complete the challenge to display email addresses
+
 // this is my solution. For once i felt that i have managed to make something that seems to make somewhat sense without
 // being a total hackjob. I think this might be my first feeling of solving a problem and damn it feels good!
 
@@ -1710,3 +1713,120 @@ string[,] corporate =
 
 
 #endregion
+
+
+
+
+#region Exercise - Complete the challenge to add methods to make the game playable
+
+// had some trouble with the console output writing some lines twice. Found out that i had put two messages in two methods. Whoopsie, that took a while for nothing lmao.
+//Anyways heres my solution
+Random random = new Random();
+
+Console.WriteLine("Would you like to play? (Y/N)");
+if (ShouldPlay())
+{
+	PlayGame();
+}
+
+void PlayGame()
+{
+	var play = true;
+
+	while (play)
+	{
+		int target = random.Next(1,6);
+		int roll = random.Next(1,7);
+
+		Console.WriteLine(WinOrLose(target, roll));
+		Console.WriteLine("\nPlay again? (Y/N)");
+		play = ShouldPlay();
+	}
+}
+
+bool ShouldPlay()
+{
+	while(true)
+	{
+		string? choice = Console.ReadLine().ToLower();
+		if (choice == "y")
+		{
+			return true;
+		}
+		else if (choice == "n")
+		{
+			return false;
+		}
+		else
+		{
+			Console.WriteLine("Invalid input please enter Y or N");
+		}
+	}
+}
+
+string WinOrLose(int targetNumber, int rollThrow)
+{
+	Console.WriteLine($"Roll a number greater than {targetNumber} to win!");
+	Console.WriteLine($"You rolled a {rollThrow}");
+
+	return (rollThrow > targetNumber) ? "you win!" : "You Lose!";
+	
+}
+
+//and heres the one from microsoft:
+
+Random random = new Random();
+
+Console.WriteLine("Would you like to play? (Y/N)");
+if (ShouldPlay()) 
+{
+	PlayGame();
+}
+
+bool ShouldPlay() 
+{
+	string response = Console.ReadLine();
+	return response.ToLower().Equals("y");
+}
+
+void PlayGame() 
+{
+	var play = true;
+
+	while (play) {
+		var target = GetTarget();
+		var roll = RollDice();
+
+		Console.WriteLine($"Roll a number greater than {target} to win!");
+		Console.WriteLine($"You rolled a {roll}");
+		Console.WriteLine(WinOrLose(roll, target));
+		Console.WriteLine("\nPlay again? (Y/N)");
+
+		play = ShouldPlay();
+	}
+}
+
+int GetTarget() 
+{
+	return random.Next(1, 6);
+}
+
+int RollDice() 
+{
+	return random.Next(1, 7);
+}
+
+string WinOrLose(int roll, int target) 
+{
+	if (roll > target) 
+	{
+		return "You win!";
+	}
+	return "You lose!";
+}
+
+#endregion
+
+
+
+
